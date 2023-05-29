f277097713262c38caa17642702ecfdba8e48da2
GyuHyeonWkdWkdMan <nero0213@naver.com>
2023-05-29 19:14:22 +0900

    First Chapter (Last Update : 29/5/2023)

--- 1/_onmyown.py
+++ 1/_onmyown.py
@@ -1,15 +1,32 @@
+from enum import Enum
+
 from fastapi import FastAPI
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
 @app.get("/")
-async def rood():
+async def root():
     return{"message": "This is the last program of Chapter 1"}
 
 @app.get("/users/me")
 async def read_user_me():
     return {"user_id": "the current user"}
 
-app.get("/users/{user_id}")
+@app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Lerning FTW!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "Le CNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
